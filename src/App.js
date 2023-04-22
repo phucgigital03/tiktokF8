53e98197d32d6bbe1c458d4bc872a356cb8ddfd9
phucnguyen <phucnguyen5102003@gmail.com>
2023-04-22 10:35:07 +0700

    config prettier and reset css with normalise.css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-
 function App() {
-  
-  return (
-      <div style={{padding: '20px'}} className="App">
-          
-      </div>
-  );
+    return (
+        <div style={{ padding: '20px' }} className="App">
+            <h1>hello anh em</h1>
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import GlobalStyles from './components/GlobalStyles';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
+    // <React.StrictMode>
     <Router>
-      <App />
-    </Router>
-  // </React.StrictMode>
+        <GlobalStyles>
+            <App />
+        </GlobalStyles>
+    </Router>,
+    // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
